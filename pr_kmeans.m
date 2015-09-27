c4596727d1fd5a5480d9aa444d535bd675f0f2cb
Rohit <rohit21122012@gmail.com>
2015-09-27 12:56:06 +0530

    Added Kmeans

---
+++ pr_kmeans.m
@@ -0,0 +1,57 @@
+function [ clusters, J ] = pr_kmeans( X , K )
+%Implementation of kmeans clustering algo
+%   Given a N*P where N is data points and P are the variable(attribuete)
+%   values, and K is number of expected of clusters, pr_kmeans returns K*P
+%   centroids for the cluster. KMeans uses the Expectation maximization to 
+%   estimate the centroids.
+    
+    N = size(X,1);
+    P = size(X,2);
+   
+    
+    
+    clusters = X(N-K+1:end,:);
+    sq_euc_dist = @(x,y) sum((x-y).^2);
+    
+    iterations = 50;
+    i = 0;
+      
+   
+    prevJ = inf;
+    while i < iterations
+        %E-step For each point in x calculate distance and assign 1 to the 
+        %minimum
+        R = zeros(N,K); %Indicator that nth point lies in kth cluster
+        J = 0;
+        for n = 1:N
+           min_dist = inf;
+           min_dist_ind = 1;
+           for k = 1:K
+              distance = sq_euc_dist(X(n,:),clusters(k,:));
+              if(distance < min_dist) 
+                min_dist = distance;
+                min_dist_ind = k;
+              end
+           end
+           R(n,min_dist_ind) = 1;
+           J = J + min_dist;
+        end
+        J
+        if (abs(prevJ - J)) < 0.0001
+            break;
+        end
+        prevJ = J;
+      
+        
+        %M-step Make the cluster center as the mean of the points
+        %assigned to the center k
+        for k = 1:K
+            clusters(k,:) = (R(:,k)' * X)/(sum(R(:,k)));
+        end
+        
+      
+       
+        i = i + 1;
+    end
+end
+
